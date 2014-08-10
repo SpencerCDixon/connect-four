a43ede6a91e2d5b90a6f67650d22a442921b192e
Spencer Dixon <spencercdixon@gmail.com>
2014-08-10 14:03:52 -0400

    Finished and working version of Connect 4.

--- board.rb
+++ board.rb
@@ -2,10 +2,11 @@ require 'pp'
 require_relative 'player'
 
 class Board
-  attr_accessor :gameboard
+  attr_accessor :gameboard, :player1, :player2
 
   def initialize
     @player1 = Player.new
+    puts
     @player2 = Player.new
     @gameboard = self.build
   end
@@ -24,26 +25,101 @@ class Board
     pp @gameboard
   end
 
-  def take_turn(player)
-    puts "Where do you want to go? Columns 1-7"
+
+  def take_turn1
+    puts "#{player1.name.capitalize}'s turn. Which column would you like to go?'"
     answer = gets.chomp.to_i
 
       if gameboard[6][answer - 1] == "O"
-        gameboard[6][answer - 1] = player.symbol
+        gameboard[6][answer - 1] = player1.symbol
       elsif gameboard[5][answer-1] == "O"
-        gameboard[5][answer - 1] = player.symbol
+        gameboard[5][answer - 1] = player1.symbol
       elsif gameboard[4][answer-1] == "O"
-        gameboard[4][answer - 1] = player.symbol
+        gameboard[4][answer - 1] = player1.symbol
       elsif gameboard[3][answer-1] == "O"
-        gameboard[3][answer - 1] = player.symbol
+        gameboard[3][answer - 1] = player1.symbol
       elsif gameboard[2][answer-1] == "O"
-        gameboard[2][answer - 1] = player.symbol
+        gameboard[2][answer - 1] = player1.symbol
       elsif gameboard[1][answer-1] == "O"
-        gameboard[1][answer - 1] = player.symbol
+        gameboard[1][answer - 1] = player1.symbol
       elsif gameboard[0][answer-1] == "O"
-        gameboard[0][answer - 1] = player.symbol
+        gameboard[0][answer - 1] = player1.symbol
+      else
+        puts "cant go there"
+        take_turn1
       end
   end
+  def take_turn2
+    puts "#{player2.name.capitalize}'s turn. Which column would you like to go?'"
+    answer = gets.chomp.to_i
 
+      if gameboard[6][answer - 1] == "O"
+        gameboard[6][answer - 1] = player2.symbol
+      elsif gameboard[5][answer-1] == "O"
+        gameboard[5][answer - 1] = player2.symbol
+      elsif gameboard[4][answer-1] == "O"
+        gameboard[4][answer - 1] = player2.symbol
+      elsif gameboard[3][answer-1] == "O"
+        gameboard[3][answer - 1] = player2.symbol
+      elsif gameboard[2][answer-1] == "O"
+        gameboard[2][answer - 1] = player2.symbol
+      elsif gameboard[1][answer-1] == "O"
+        gameboard[1][answer - 1] = player2.symbol
+      elsif gameboard[0][answer-1] == "O"
+        gameboard[0][answer - 1] = player2.symbol
+      else
+        puts "Can't go there"
+        take_turn2
+      end
+  end
+  def show_board_guide
+    puts
+    puts "In order to take your turn write a number 1-7."
+    puts "It will drop your symbol in place."
+    puts "[1,2,3,4,5,6,7] shows the different columns"
+    puts
+  end
+  def check_for_win
+
+  end
+
+end
+
+
+# Experimenting with trying to figure out to check if someone wins
+def array_size(array, x, y)
+  if array[y][x] != 'X'
+    return 0
+  end
+
+  size = 1
+  array[y][x] = 'counted tile'
+
+  size = size + array_size(array, x-1, y-1)
+  size = size + array_size(array, x, y-1)
+  size = size + array_size(array, x+1, y-1)
+  size = size + array_size(array, x-1, y)
+  size = size + array_size(array, x+1, y)
+  size = size + array_size(array, x-1, y+1)
+  size = size + array_size(array, x, y+1)
+  size = size + array_size(array, x+1, y+1)
+
+  size
+
+end
+
+[["O","O","O","O","O","O","O"],
+ ["O","O","O","O","O","O","O"],
+ ["O","O","O","O","O","O","O"],
+ ["O","O","O","X","O","O","O"],
+ ["X","O","O","X","O","O","O"],
+ ["X","O","O","X","O","O","O"],
+ ["X","O","O","O","O","O","O"]]
+
+def check_column(num)
+  counter = 0
+  num2 = 0
+  if gameboard[num2][num] == 'O'
+    
 
 end
--- play_game.rb
+++ play_game.rb
@@ -1,2 +1,20 @@
 require_relative 'player'
 require_relative 'board'
+
+
+connect4 = Board.new
+while true
+
+# Shows board Initially
+connect4.show_board_guide
+connect4.show_board
+sleep(1)
+# Player 1 takes turn
+connect4.take_turn1
+connect4.show_board
+# Player 2 takes turn
+connect4.take_turn2
+connect4.show_board
+
+
+end
